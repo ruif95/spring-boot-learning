ee5cf96d9c61e0b72ce6a52bb116f4e69478d3b8
Rui Ferreira <ruif95jp@gmail.com>
2025-07-23 16:20:34 +0100

    Add spring boot skeleton

---
+++ src/test/java/com/example/spring_boot_learning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
